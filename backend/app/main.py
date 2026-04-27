c752c21b698f8577cebcfbf1efda93a503d6355f
femi123456 <akinpeluoluwafemidavid@gmail.com>
2026-04-27 22:41:54 +0100

    fix: fixed accesss origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080",
         "http://localhost:5173",
-        "https://crisis-awareness-booking-system.vercel.app"
+        "https://crisis-awareness-booking-system.vercel.app",
+        "https://www.crisis-awareness-booking-system.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
